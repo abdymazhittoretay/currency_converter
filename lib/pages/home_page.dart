24ab9e177a24727451365b66a309adaacc7508c8
Toretay <abdymvzhit@gmail.com>
2025-02-13 21:50:45 +0500

    changed styling of appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,11 +17,18 @@ class _HomePageState extends State<HomePage> {
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   "Currency\nConverter",
-                  style: TextStyle(fontSize: 36.0, color: Colors.blueAccent),
+                  style: TextStyle(
+                    fontSize: 36.0,
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
+                SizedBox(height: 24.0),
               ],
             ),
           ),
