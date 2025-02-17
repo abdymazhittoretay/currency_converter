ab0c526b8c018504908f3c0d2d8534cd9ef9c93a
Toretay <abdymvzhit@gmail.com>
2025-02-17 20:32:02 +0500

    added button to convert

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -101,6 +101,23 @@ class _HomePageState extends State<HomePage> {
                             ),
                           ),
                         ),
+                        SizedBox(height: 40.0),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            backgroundColor: Colors.blueAccent,
+                            foregroundColor: Colors.white,
+                            minimumSize: Size(
+                              MediaQuery.sizeOf(context).width,
+                              80,
+                            ), // Full width & height
+                            textStyle: TextStyle(fontSize: 24.0),
+                          ),
+                          onPressed: () {},
+                          child: Text("Convert"),
+                        ),
                       ],
                     )
                     : Center(
